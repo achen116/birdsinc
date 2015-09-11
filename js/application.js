b7bf0124d5af76af126a6ac8888125148582d7bf
achen116 <chen.arielle@gmail.com>
2015-09-10 22:19:29 -0700

    implement jquery ticker with bxslider - need to add styling

---
+++ js/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.bxslider').bxSlider({
+	  minSlides: 4,
+	  maxSlides: 4,
+	  slideWidth: 500,
+	  slideMargin: 100,
+	  ticker: true,
+	  speed: 100000
+	});
+});
