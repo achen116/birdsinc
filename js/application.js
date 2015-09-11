3652aa76505e0e4b7834650b45f653cd67abba23
achen116 <chen.arielle@gmail.com>
2015-09-11 09:51:15 -0700

    add hover feature for bird gallery, refactor bxslider into separate function

--- js/application.js
+++ js/application.js
@@ -1,13 +1,30 @@
-$(document).ready(function() {
-	$('.bxslider').bxSlider({
-	  minSlides: 4,
-	  maxSlides: 4,
-	  slideWidth: 500,
-	  // slideMargin: 10,
-	  ticker: true,
-	  speed: 100000
+(function() {
+	$(document).ready(function() {
+		ticker();
+		hover();
+
+		$('.bird-img').colorbox({close: "x"});
 	});
 
-	$('.bird-img').colorbox({close: "x"});
+	var ticker = function() {
+		$('.bxslider').bxSlider({
+		  minSlides: 4,
+		  maxSlides: 4,
+		  slideWidth: 500,
+		  ticker: true,
+		  speed: 100000
+		});
+	}
 
-});
+	var hover = function() {
+		$('.bird-img').hover(
+			function() {
+		    $(this).find('img').css('opacity', 0.4);
+			},
+			function() {
+		    $(this).find('img').css('opacity', 1);
+			}
+		);
+	}
+
+})();
